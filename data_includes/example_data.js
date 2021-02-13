b342654c6975c907c070535ebe9ef1215a7dccc1
iki1997 <79021323+iki1997@users.noreply.github.com>
2021-02-13 20:18:53 +0100

    Update example_data.js

--- data_includes/example_data.js
+++ data_includes/example_data.js
@@ -1,19 +1,19 @@
 var shuffleSequence = seq(anyType);
 
     var defaults = [
-        "Question", {as: ["Yes", "No"]},
+        "Question", {as: ["1", "2", "3", "4", "5"]},
         "DashedSentence", {mode: "speeded acceptability"}
     ];
 
     var items = [
 
-    ["filler", "DashedSentence", {s: "ALOOO BEEE RADIS LI???"},
-               "Question", {q: "Is this a filler sentence?"}],
-    ["filler", "DashedSentence", {s: "And another silly filler sentence"},
-               "Question", {q: "Does this sentence have a relative clause?"}],
-    ["relclause", "DashedSentence", {s: "A sentence that has a relative clause"},
-                  "Question", {q: "Was there movement through [Spec,CP]?"}],
-    ["relclause", "DashedSentence", {s: "Another sentence that has a relative clause"},
-                  "Question", {q: "Was the first word of that sentence 'frog'?"}]
+    ["filler", "DashedSentence", {s: "Pročitao sam dobru knjigu."},
+               "Question", {q: "Koliko je prihatljiva ova rečenica?"}],
+    ["filler", "DashedSentence", {s: "Doneo je izgleda poklone."},
+               "Question", {q: "Koliko je prihatljiva ova rečenica?"}],
+    ["relclause", "DashedSentence", {s: "Trčala brzo je Marija kroz šumu."},
+                  "Question", {q: "Koliko je prihatljiva ova rečenica?"}],
+    ["relclause", "DashedSentence", {s: "Hodala polako je Marija kroz šumu"},
+                  "Question", {q: "Koliko je prihatljiva ova rečenica?"}]
 
     ];
