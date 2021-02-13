97ee953f8d6bf298aa1cf4992be9f49eb0b7ddd7
iki1997 <79021323+iki1997@users.noreply.github.com>
2021-02-13 20:21:07 +0100

    Update example_data.js

--- data_includes/example_data.js
+++ data_includes/example_data.js
@@ -1,7 +1,7 @@
 var shuffleSequence = seq(anyType);
 
     var defaults = [
-        "Question", {as: ["1", "2", "3", "4", "5"]},
+        "Question", {as: ["uvek neprihvatljiva", "neprihvatljiva", "ponekad prihvatljiva", "prihvatljiva", "uvek prihvatljiva"]},
         "DashedSentence", {mode: "speeded acceptability"}
     ];
 
@@ -11,9 +11,9 @@ var shuffleSequence = seq(anyType);
                "Question", {q: "Koliko je prihatljiva ova rečenica?"}],
     ["filler", "DashedSentence", {s: "Doneo je izgleda poklone."},
                "Question", {q: "Koliko je prihatljiva ova rečenica?"}],
-    ["relclause", "DashedSentence", {s: "Trčala brzo je Marija kroz šumu."},
+    ["stimulus", "DashedSentence", {s: "Trčala brzo je Marija kroz šumu."},
                   "Question", {q: "Koliko je prihatljiva ova rečenica?"}],
-    ["relclause", "DashedSentence", {s: "Hodala polako je Marija kroz šumu"},
+    ["stimulus", "DashedSentence", {s: "Hodala polako je Marija kroz šumu"},
                   "Question", {q: "Koliko je prihatljiva ova rečenica?"}]
 
     ];
